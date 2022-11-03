06b36702a7033c28c070972edb0c316c90702c8a
Kans-alpha <40798270+Kans-alpha@users.noreply.github.com>
2022-11-03 16:27:52 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,69 +1,23 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
+variable "instance_name" {}
+variable "instance_zone" {}
+variable "instance_type" {
+  default = "n1-standard-1"
   }
-}
-provider "google" {
-  version = "3.5.0"
-  project = "qwiklabs-gcp-00-04386a91e806"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
+variable "instance_network" {}
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-   tags         = ["web", "dev", "koga", "hoga", "googlehoga"]
-
-   provisioner "local-exec" {
-    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
-  }
+  name         = "${var.instance_name}"
+  zone         = "${var.instance_zone}"
+  machine_type = "${var.instance_type}"
   boot_disk {
     initialize_params {
-       image = "cos-cloud/cos-stable"
-    }
-  }
-    network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-      nat_ip = google_compute_address.vm_static_ip.address
-    }
-  }
-}
-
-resource "google_compute_address" "vm_static_ip" {
-  name = "terraform-static-ip"
-}
-
-# New resource for the storage bucket our application will use.
-resource "google_storage_bucket" "example_bucket" {
-  name     = "bucket-qwiklabs-gcp-00-04386a91e806"
-  location = "EU"
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
-# Create a new instance that uses the bucket
-resource "google_compute_instance" "another_instance" {
-  # Tells Terraform that this VM instance must be created only after the
-  # storage bucket has been created.
-  depends_on = [google_storage_bucket.example_bucket]
-  name         = "terraform-instance-2"
-  machine_type = "f1-micro"
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
+      image = "debian-cloud/debian-9"
+      }
   }
   network_interface {
-    network = google_compute_network.vpc_network.self_link
+    network = "${var.instance_network}"
     access_config {
+      # Allocate a one-to-one NAT IP to the instance
     }
   }
 }
